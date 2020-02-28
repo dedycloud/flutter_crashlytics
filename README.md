68dd66f06e6e2affe385c75965da383c88e16a3e
dedycloud <dedyindra351@gmail.com>
2020-02-29 00:59:14 +0700

    [DOCS] add file readme.md

--- README.md
+++ README.md
@@ -1,16 +1,45 @@
 # flutter_x_crashlytics
 
-A new Flutter project.
+This is a demo app and containing some simple `scenario for crash reporting using Crashlytics tool by Firebase.`
+
+##Crash
+`crash` adalah satu keadaan di mana sebuah komputer atau program, baik aplikasi atau bagian dari suatu sistem operasi berhenti berfungsi, umumnya akan menutup atau keluar setelah menghadapi kesalahan. 
+
+Ketika Mobile apps sudah dirilis ke publik dan masih memiliki suatu bug atau issue yang menyebabkan Mobile apps tersebut crash dapat menyebabkan pengguna meng-uninstal atau bahkan menulis review negatif pada App Store atau mungkin akan mempostingnya di sosial media.
+
+##Crashlytics
+Firebase `Crashlytics` merupakan solusi pelaporan yang ringan dan dapat bekerja untuk membantu pelacakan crash dengan jelas dan dapat ditindaklanjuti dari masalah aplikasi, dengan solusi pelaporan yang andal untuk Android dan iOS. Selain membantu pelacakan crash, Firebase Crashlytics dapat membantu memprioritaskan serta memperbaiki masalah stabilitas yang mengurangi kualitas Mobile apps. Dengan menggunakan Firebase Crashlytics dapat menghemat waktu dan effort Pemecahan masalah dengan mengelompokkan error secara cerdas dan menunjukkan keadaan yang menyebabkannya.
+Masalah dikelompokkan berdasarkan method dan line exception yang dibuat, dengan peringatan bahwa itu adalah akar penyebab dari sebuah exception.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Here's to help you with Flutter : 
+- [Flutter Installation](https://flutter.dev/docs/get-started/install)
 
-A few resources to get you started if this is your first Flutter project:
+Once Flutter is set on your PC, you will need to get firebase and crashlytics added into your Flutter projects :
+- [Add Firebase to Flutter Projects](https://firebase.google.com/docs/flutter/setup?platform=android)
+- [Add Crashlytics to Flutter Projects](https://pub.dev/packages/firebase_crashlytics)
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+Here's some helping links :
+- [Official sample flutter project from firebase](https://github.com/FirebaseExtended/flutterfire/tree/master/packages/firebase_crashlytics/example)
+- [Find keystore and add it to firebase project](https://codelabs.developers.google.com/codelabs/firebase-android/#4)
+- [Stackoverflow : Flutter Crashlytics log caught exception](https://stackoverflow.com/questions/57997416/flutter-crashlytics-log-caught-exception)
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+##Hal yang perlu di `highlight` saat memeriksa report crash 
+- Seberapa sering bug terjadi => bisa dilihat dari apakah crash terjadi pada sekali dari 1.000 pengguna, berapa kali peristiwa terjadi dan terpengaruh ke berapa pengguna saat di klik page tertentu
+- Perangkat apa yang digunakan saat terjadi crash 
+Termasuk kategori apa crash yang terjadi => fatal atau non fatal
+```
+Fatal    : crash prioritas harus di perbaiki karena berpengaruh kepada pengguna membuat ketidaknyamanan,
+Nonfatal : crash tidak terlalu parah namun mungkin ada solusinya. Ini harus diperbaiki di beberapa titik.
+```
+- Nama project dan packagename, termasuk nomor baris tempat crash terjadi
+- Jumlah total crash
+- Jumlah total pengguna yang terpengaruh oleh crash
+##Example result
+![crash](kliks.png)
+contoh kasus disini adalah error out of bound => mencoba mencetak data array yang tidak mempunyai nilai index  
+#####crash reporting 
+![crash](throw.png)
+##apa yang harus di lakukan ?
+share the issue via social-media or with your team members
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-  import 'dart:async';
+import 'dart:async';
 
 import 'dart:io';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -8,7 +8,6 @@ import 'package:stack_trace/stack_trace.dart';
 
 void main() {
   Crashlytics.instance.enableInDevMode = true;
-//  Crashlytics.instance.setUserIdentifier('123');
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runZoned(() {
@@ -26,28 +25,30 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
   }
+
   final List<int> list = <int>[];
 
   String name = '';
-  int number=0;
+  int number = 0;
 
   changeName(nameInput) {
-    if(name==null){
-        throw IOException;
+    if (name == null) {
+      throw IOException;
     }
     setState(() {
       this.name = nameInput;
     });
   }
 
-  changeNumber(numberGiven){
-    if(number==0){
+  changeNumber(numberGiven) {
+    if (number == 0) {
       throw new IntegerDivisionByZeroException();
     }
     setState(() {
-      number=numberGiven;
+      number = numberGiven;
     });
   }
+
   void loop() {
     for (int b = 1; b > 0; b++) {
       print(list[-1]);
@@ -64,55 +65,67 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             children: <Widget>[
-              RaisedButton(
-                  color: Colors.blue,
-                  child: const Text('change state'),
-                  onPressed: () {
-                    try {
-                      changeName(null);
-                    } catch (exception, stack) {
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                    color: Colors.blue,
+                    child: const Text('change state'),
+                    onPressed: () {
+                      try {
+                        changeName(null);
+                      } catch (exception, stack) {
+                        print(DateTime.now());
+                        Crashlytics.instance.setString("fatal", exception);
+                        Crashlytics.instance.log("error state");
+                        Crashlytics.instance.recordError(exception, stack,
+                            context: 'error change state');
+                      }
+                    }),
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                    color: Colors.teal,
+                    child: const Text('change number'),
+                    onPressed: () {
+                      try {
+                        changeNumber(0);
+                      } catch (exception, stack) {
+                        print(DateTime.now());
+                        Crashlytics.instance.recordError(exception, stack,
+                            context: 'error change number');
+                        Crashlytics.instance.log("change number");
+                      }
+                    }),
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                    child: const Text('Async out of bounds'),
+                    color: Colors.blue,
+                    onPressed: () {
                       print(DateTime.now());
-                      Crashlytics.instance.setString("fatal", exception);
-                      Crashlytics.instance.log("error state");
-                      Crashlytics.instance.recordError(exception, stack,
-                          context: 'error change state');
-                    }
-                  }),
-              RaisedButton(
-                color: Colors.teal,
-                  child: const Text('change number'),
-                  onPressed: () {
-                    try {
-                      changeNumber(0);
-                    } catch (exception, stack) {
-                      print(DateTime.now());
-                      Crashlytics.instance.recordError(exception, stack,
-                          context: 'error change number');
-                      Crashlytics.instance.log("change number");
-                    }
-                  }),
-              RaisedButton(
-                  child: const Text('Async out of bounds'),
-                  color: Colors.blue,
-                  onPressed: () {
-                    print(DateTime.now());
-                    print('Async out of bounds');
-                    Future<void>.delayed(const Duration(seconds: 2), () {
-                      final List<int> list = <int>[];
-                      print(list[90]);
-                    });
-                  }),
-              RaisedButton(
-                  color: Colors.amber,
-                  child: const Text('Trhow Record Error'),
-                  onPressed: () {
-                    try {
-                      throw 'error_example';
-                    } catch (exception, stack) {
-                      Crashlytics.instance.recordError(exception, stack,
-                          context: 'error as an example');
-                    }
-                  }),
+                      print('Async out of bounds');
+                      Future<void>.delayed(const Duration(seconds: 2), () {
+                        final List<int> list = <int>[];
+                        print(list[90]);
+                      });
+                    }),
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                    color: Colors.amber,
+                    child: const Text('Trhow Record Error'),
+                    onPressed: () {
+                      try {
+                        throw 'error_example';
+                      } catch (exception, stack) {
+                        Crashlytics.instance.recordError(exception, stack,
+                            context: 'error as an example');
+                      }
+                    }),
+              ),
             ],
           ),
         ),
